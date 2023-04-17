25b5c0b25684785b43f318adcb4fb1486321b772
foysalkazi11 <foysalkazi11@gmail.com>
2023-04-18 00:25:23 +0600

    first commit

---
+++ my-website/assets/js/modal.js
@@ -0,0 +1,24 @@
+var modal = document.getElementById('modal');
+var overlay = document.getElementById('overlay');
+var popup_close_btn = document.querySelector('.popup__close button');
+
+window.addEventListener('load', () => {
+  var params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  var submitted = params.submitted;
+  if (submitted === 'true') {
+    modal.style.display = 'block';
+  }
+});
+
+overlay.addEventListener('click', () => {
+  modal.style.display = 'none';
+  window.history.replaceState({}, document.title, '/' + '');
+});
+
+popup_close_btn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  window.history.replaceState({}, document.title, '/' + '');
+});
