f3fd0c4122b13e06887707033066f57e5d25f765
foysalkazi11 <foysalkazi11@gmail.com>
2023-04-18 12:41:54 +0600

    contrat us page bug fixed

--- my-website/assets/js/modal.js
+++ my-website/assets/js/modal.js
@@ -1,24 +1,25 @@
-var modal = document.getElementById('modal');
-var overlay = document.getElementById('overlay');
-var popup_close_btn = document.querySelector('.popup__close button');
+var modal = document.getElementById("modal");
+var overlay = document.getElementById("overlay");
+var popup_close_btn = document.querySelector(".popup__close button");
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   var params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
+  console.log(params.submitted);
 
   var submitted = params.submitted;
-  if (submitted === 'true') {
-    modal.style.display = 'block';
+  if (submitted === "true") {
+    modal.style.display = "block";
   }
 });
 
-overlay.addEventListener('click', () => {
-  modal.style.display = 'none';
-  window.history.replaceState({}, document.title, '/' + '');
+overlay.addEventListener("click", () => {
+  modal.style.display = "none";
+  window.history.replaceState({}, document.title, "/contract.html" + "");
 });
 
-popup_close_btn.addEventListener('click', () => {
-  modal.style.display = 'none';
-  window.history.replaceState({}, document.title, '/' + '');
+popup_close_btn.addEventListener("click", () => {
+  modal.style.display = "none";
+  window.history.replaceState({}, document.title, "/contract.html" + "");
 });
